e28aee628ad314c423dd273e5de9f1279b7c170a
elifred2022 <elifed21@hotmail.com>
2023-08-28 13:53:09 -0300

    haciendo el historial de movimeintos

--- src/App.js
+++ src/App.js
@@ -15,9 +15,9 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null)); // crea una matriz con 9 elementos y establece cada uno en nulo
+function Board({ xIsNext, squares, onPlay }) {
+  //const [xIsNext, setXIsNext] = useState(true);
+  //const [squares, setSquares] = useState(Array(9).fill(null)); // crea una matriz con 9 elementos y establece cada uno en nulo
 
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
@@ -29,9 +29,7 @@ export default function Board() {
     } else {
       nextSquares[i] = "O";
     }
-
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -64,6 +62,28 @@ export default function Board() {
   );
 }
 
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol> {/*TODO*/} </ol>
+      </div>
+    </div>
+  );
+}
+
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
@@ -85,4 +105,4 @@ function calculateWinner(squares) {
   return null;
 }
 
-// https://es.react.dev/learn/tutorial-tic-tac-toe#setup-for-the-tutorial ¡Felicidades! Ahora tienes un juego de Tres en línea que funciona.
+// https://es.react.dev/learn/tutorial-tic-tac-toe#setup-for-the-tutorial Utilizarás map para transformar tu matriz de movimientos history
